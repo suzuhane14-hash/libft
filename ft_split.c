26153944b4b6c025aae512f8596a2ac6af99e3fd
suzuhane14-hash <suzuhane14@gmail.com>
2026-05-04 15:50:39 +0900

    10

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: omito <omito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/03 22:47:02 by omito             #+#    #+#             */
-/*   Updated: 2026/05/03 23:06:51 by omito            ###   ########.fr       */
+/*   Updated: 2026/05/04 15:49:40 by omito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,22 @@ char	**ft_split(char const *s, char c)
 	while (s[i] != '\0')
 	{
 		while (s[i] != c)
-			i++;
-		counter++;
-		str[j] = malloc(counter + 1);
+		{	i++;
+			char_counter++;	
+		}	
+		words_counter++;
+		str[j] = malloc(char_counter + 1);
+		
+		if (!str[j])
+			return (NULL);
+		j++;
+	}
+	return (
+}
 
-			
+int main(void)
+{
+	char const *S = "asdfghjk";
+	char s = 'g';
+
+
