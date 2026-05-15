7e10c430970f5aa7e1ffd1e9f4bbdca96135bea9
suzuhane14-hash <suzuhane14@gmail.com>
2026-05-15 19:24:25 +0900

    re_ctype.h

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,12 +6,11 @@
 /*   By: omito <omito@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 15:27:07 by omito             #+#    #+#             */
-/*   Updated: 2026/05/15 15:56:53 by omito            ###   ########.fr       */
+/*   Updated: 2026/05/15 19:19:12 by omito            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
